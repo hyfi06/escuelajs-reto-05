82b8dd60a6cc00f23b538600c3769f805ce4afbd
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2019-09-28 12:37:04 -0500

    Retos completados, sin asing ='(

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
-const API = 'https://rickandmortyapi.com/api/character/';
+const API = 'https://us-central1-escuelajs-api.cloudfunctions.net/characters';
+const KEY_NEXT_FETCH = 'next_fetch';
+
+const cleanLocalStorage = function () {
+    localStorage.removeItem(KEY_NEXT_FETCH);
+}
 
 const getData = api => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
       const characters = response.results;
+      const next_page = response.info.next;
+
+      localStorage.setItem(KEY_NEXT_FETCH,next_page);
+
       let output = characters.map(character => {
         return `
       <article class="Card">
@@ -24,7 +33,21 @@ const getData = api => {
 }
 
 const loadData = () => {
-  getData(API);
+  let url;
+  const next_fetch = localStorage.getItem(KEY_NEXT_FETCH);
+  if ( typeof next_fetch === 'string' && next_fetch.length !== 0 ) {
+    url = next_fetch;
+  } else if ( typeof next_fetch === 'string' && next_fetch.length == 0 ){
+    detenerObserver();
+  } else {
+    url = API;
+  }
+  getData(url);
+}
+
+const detenerObserver = function () {
+  intersectionObserver.unobserve($observe);
+  $observe.innerHTML='Ya no hay más personajes';
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
@@ -34,5 +57,7 @@ const intersectionObserver = new IntersectionObserver(entries => {
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
+
+cleanLocalStorage();
 
 intersectionObserver.observe($observe);
